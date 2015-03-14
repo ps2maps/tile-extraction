c1624a77862914f872c40b6e46b882e95920c4ca
Jake Wilson <jake.e.wilson@gmail.com>
2015-03-14 00:06:00 -0600

    Switched to iterating over each continent instead of all continents at once. Added Zoom Level 4 tile creation via montage.

--- extract_tiles.py
+++ extract_tiles.py
@@ -37,91 +37,118 @@ def ps2int(num):
 class entry:
 	pass
 
-root=sys.path[0]+"/"
-targetfolder=root+"output/"
+output = sys.path[0] + "/output/"
 
 # PlanetSide 2 installation Assets folder
 inputfolder = "C:/PlanetSide2/Resources/Assets/"
 
-# ImageMagick Convert exe
+# JPG Quality
+quality = str(85)
+
+# ImageMagick exe's
 convert = "C:/ImageMagick/convert.exe"
+montage = "C:/ImageMagick/montage.exe"
 
 # List of continents to extract tiles for
 continents = ['indar']
 
-if os.path.exists(targetfolder):
-	print "Deleting output folder"
-	shutil.rmtree(targetfolder)
-time.sleep(1)
-os.mkdir(targetfolder)
+# Loop thru continents
+for continent in continents:
 
-print "Extracting map tiles",
-if len(continents) > 0:
-	print "for " + ", ".join(continents)
+	# Delete and recreate output folder
+	if os.path.exists(output + continent):
+		print "Deleting output folder for " + continent
+		shutil.rmtree(output + continent)
+		time.sleep(1)
 
-ij=0
-while 1:
-	partSize=1 #anything non-zero to make a do-while
-	entrylist=[]
-	fname=inputfolder+"Assets_"+ps2int(ij)+".pack"
-	if not os.path.exists(fname):
-		break
-	pack = open(fname,"rb")
-	while partSize:
-		partSize=unpack(">I",pack.read(4))[0]
-		numEntries=unpack(">I",pack.read(4))[0]
-		for i in xrange(numEntries):
-			ent=entry()
-			stringlen=unpack(">I",pack.read(4))[0]
-			ent.name=pack.read(stringlen)
-			ent.offset,ent.size,ent.crc32=unpack(">III",pack.read(12))
-			entrylist.append(ent)
+	print "Extracting zoom level 5 tiles for " + continent
 
-		end=entrylist[-1].offset+entrylist[-1].size
-		pack.seek(partSize)
+	# Create output folders
+	zoom5 = output + continent + "/zoom5/"
+	os.makedirs(zoom5)
+	zoom4 = output + continent + "/zoom4/"
+	os.makedirs(zoom4)
+	zoom3 = output + continent + "/zoom3/"
+	os.makedirs(zoom3)
+	zoom2 = output + continent + "/zoom2/"
+	os.makedirs(zoom2)
+	zoom1 = output + continent + "/zoom1/"
+	os.makedirs(zoom1)
+	zoom0 = output + continent + "/zoom0/"
+	os.makedirs(zoom0)
 
-	for ent in entrylist:
-		pack.seek(ent.offset)
-		pathinfo = os.path.splitext(ent.name)
-		extension = pathinfo[1][1:].lower()
-		filename = pathinfo[0].lower()
+	ij=0
+	while 1:
+		partSize=1 #anything non-zero to make a do-while
+		entrylist=[]
+		fname=inputfolder+"Assets_"+ps2int(ij)+".pack"
+		if not os.path.exists(fname):
+			break
+		pack = open(fname,"rb")
+		while partSize:
+			partSize=unpack(">I",pack.read(4))[0]
+			numEntries=unpack(">I",pack.read(4))[0]
+			for i in xrange(numEntries):
+				ent=entry()
+				stringlen=unpack(">I",pack.read(4))[0]
+				ent.name=pack.read(stringlen)
+				ent.offset,ent.size,ent.crc32=unpack(">III",pack.read(12))
+				entrylist.append(ent)
 
-		# Find map tiles
-		if extension == 'dds' and filename.find('_lod0') != -1:
+			end=entrylist[-1].offset+entrylist[-1].size
+			pack.seek(partSize)
 
-			if len(continents) > 0:
-				# Find specified continents
-				continentFound = False
-				for continent in continents:
-					if filename.find(continent.lower()) != -1:
-						continentFound = True
-						break
-				if continentFound == False:
-					continue;
+		for ent in entrylist:
+			pack.seek(ent.offset)
+			pathinfo = os.path.splitext(ent.name)
+			extension = pathinfo[1][1:].lower()
+			filename = pathinfo[0].lower()
 
-			print '.',
+			# Find map tiles
+			if extension == 'dds' and filename.find('_lod0') != -1 and filename.find(continent) != -1:
 
-			# Convert the tile coordinates
-			fileparts = filename.replace('_lod0','').replace('_tile','').split('_')
-			continent = fileparts[0]
-			longitude = str(int(fileparts[1])/4)
-			latitude = str((int(fileparts[2]) * -1)/4 - 1)
+				print '.',
 
-			# Output filename (zoom level 5 is base zoom value)
-			outputFilename = "tile_5_" + longitude + "_" + latitude + ".jpg"
+				# Convert the tile coordinates
+				fileparts = filename.replace('_lod0','').replace('_tile','').split('_')
+				longitude = str(int(fileparts[1])/4)
+				latitude = str((int(fileparts[2]) * -1)/4 - 1)
 
-			# Write temp DDS file
-			tmp = targetfolder + ent.name.lower()
-			dds=open(tmp,"wb")
-			dds.write(pack.read(ent.size))
-			dds.close()
+				# Output filename (zoom level 5 is base zoom value)
+				outputFilename = continent + "_5_" + longitude + "_" + latitude + ".jpg"
 
-			# Convert to PNG
-			cmd = convert + " " + tmp + " -flip -quality 85 " + targetfolder + outputFilename
-			os.system(cmd)
+				# Write temp DDS file
+				tmp = zoom5 + ent.name.lower()
+				dds=open(tmp,"wb")
+				dds.write(pack.read(ent.size))
+				dds.close()
 
-			# Delete the tmp DDS file
-			os.remove(tmp)
+				# Convert to PNG
+				cmd = convert + " " + tmp + " -flip -quality " + quality + " " + zoom5 + outputFilename
+				os.system(cmd)
 
-	pack.close()
-	ij+=1
+				# Delete the tmp DDS file
+				os.remove(tmp)
+
+		pack.close()
+		ij+=1
+
+	# Create Zoom Level 4 tiles
+	# Combines 4 tiles into one in mosaic pattern
+	print "Extracting zoom level 4 tiles for " + continent
+
+	for x in range(-16,16):
+		if x%2 == 0:
+			for y in range(-16,16):
+				if y%2 == 0:
+
+					tile1 = zoom5 + continent + "_5_" + str(x) + "_" + str(y) + ".jpg"
+					tile2 = zoom5 + continent + "_5_" + str(x+1) + "_" + str(y) + ".jpg"
+					tile3 = zoom5 + continent + "_5_" + str(x) + "_" + str(y+1) + ".jpg"
+					tile4 = zoom5 + continent + "_5_" + str(x+1) + "_" + str(y+1) + ".jpg"
+
+					outputFilename = continent + "_4_" + str(x/2) + "_" + str(y/2) + ".jpg"
+
+					cmd = montage + " -geometry +0+0 -background none -quality " + quality + " " + tile1 + " " + tile2 + " " + tile3 + " " + tile4 + " " + zoom4 + outputFilename
+					os.system(cmd)
+
